46b6a30049eb66a540899146c41420f110839fae
John Sheehy <sheehy.john@gmail.com>
2023-11-23 14:04:35 +0000

    reading files, adding to array and making notes

--- examples/node/example.markdown.js
+++ examples/node/example.markdown.js
@@ -29,38 +29,57 @@ let fileListAll = [];
 
 
 fs.promises.readdir('C:/code/obsidian/media-2307')
-  .then(files => {
-    let promises = files.map(file => {
-      if (file.endsWith('.md')) {
-        const filePath = path.join('C:/code/obsidian/media-2307', file);
-        
-        return fs.promises.readFile(filePath, 'utf-8')
-          .then(content => {
-            let fileAndContents = {
-              filename: file,
-              contents: content
-            };
+    .then(files => {
+        let promises = files.map(file => {
+        if (file.endsWith('.md')) {
+            const filePath = path.join('C:/code/obsidian/media-2307', file);
             
-            fileListAll.push(fileAndContents);
-          });
-      }
-    });
+            return fs.promises.readFile(filePath, 'utf-8')
+            .then(content => {
+                let fileAndContents = {
+                filename: file,
+                contents: content
+                };
+                
+                fileListAll.push(fileAndContents);
+            });
+        }
+        });
 
     return Promise.all(promises);
   })
-  .then(() => {
-    fileListAllConsole();
+    .then(() => {
+        fileListAllConsole();
   })
-  .catch(err => {
-    console.error(err);
+    .catch(err => {
+        console.error(err);
   });
 
 
+    // The function above has read all the markdown files in the directory and stored them in an array of objects.
+    // The objects have two properties: filename and contents.
+    // The contents property is the text of the markdown file.
+    // The filename property is the name of the markdown file.
+    // The array of objects is called fileListAll.
+    
 
+    // the function below needs to convert each element of fileListAll to a string and then pass it to the parse function.
+    // The parse function will return an array of objects.
 
 function fileListAllConsole() {
     console.log('---fileListAllConsole()---');
     console.log(fileListAll)
    
+
+    //let Markdown =
+
+    /* 
+        var text = fs.readFileSync('../data/gtor.md', 'utf-8');
+
+        var headings = parse(text);
+        var root = transform(headings);
+    */
+
+    var a = parse(fileListAll[0]);
 
 }
