28976efdb02cd76745e6703edd136855c38d1b03
John Sheehy <sheehy.john@gmail.com>
2023-11-23 16:30:16 +0000

    issue seems to be the [] containing json

--- examples/browser/example.json.js
+++ examples/browser/example.json.js
@@ -5,8 +5,9 @@ const markmap = require('../../lib/view.mindmap');
 console.log('-----');
 
 
-d3.json("../data/tree.json", function(error, data) {
-//  d3.json("../data/js-test01.json", function(error, data) {
+//d3.json("../data/tree.json", function(error, data) {
+d3.json("../data/js-test01.json", function(error, data) {
+    //d3.json("../data/js-test01-onefile.json", function(error, data) {
   if (error) throw error;
 
   markmap('svg#mindmap', data, {
